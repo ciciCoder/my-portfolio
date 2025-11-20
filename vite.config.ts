64b57117fb83328f4d5816d09d09d64de92d70cf
cicivillar@ <cicivillar@gmail.com>
2025-11-20 22:37:19 +0800

    init

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import injectHTML from 'vite-plugin-html-inject'
 export default defineConfig({
+  base: '/my-portfolio/',
   plugins: [
     tailwindcss(),
     injectHTML(),
